c18f678226714f4cc2fb56eb05abe814e3c19c42
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-14 01:02:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function aveSpd(upTime, upSpd, downSpd) {
-  // your code here
+  const totalDistance = upSpd * (upTime / 60); // Calculate total distance traveled
+
+  // Calculate total time and average speed
+  const totalTime = upTime + (totalDistance / downSpd) * 60;
+  const averageSpeed = (2 * totalDistance) / (totalTime / 60);
+
+  return averageSpeed;
 }
+console.log(aveSpd(18, 20, 60)); // Output: 30
+console.log(aveSpd(30, 10, 30)); // Output: 15
+console.log(aveSpd(30, 8, 24)); // Output: 12
+
 
 // Do not change the code below
 const upTime = prompt("Enter upTime: ");
 const downTime = prompt("Enter downTime: ");
 const downSpd = prompt("Enter downSpd: ");
 
-alert(aveSpd(upTime, downTime, downSpd));
+//alert(aveSpd(upTime, downTime, downSpd));
